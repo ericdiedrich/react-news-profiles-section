9606621a6edad267e9d659527bb25843f07d9135
ericdiedrich <ericdiedrich@users.noreply.github.com>
2019-11-21 11:45:57 +0000

    Updates

---
+++ precache-manifest.7f2bea1a6de2058472ba2d92f855aa64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf220c5da5c14337ec2a94f4adadca0",
+    "url": "/react-news-profiles-section/index.html"
+  },
+  {
+    "revision": "e03eddc2fb262c9ef25e",
+    "url": "/react-news-profiles-section/static/css/main.65de0138.chunk.css"
+  },
+  {
+    "revision": "4ca6da66db63894e2aab",
+    "url": "/react-news-profiles-section/static/js/2.04e3977c.chunk.js"
+  },
+  {
+    "revision": "e03eddc2fb262c9ef25e",
+    "url": "/react-news-profiles-section/static/js/main.34fd456f.chunk.js"
+  },
+  {
+    "revision": "5040e828d46b954976a3",
+    "url": "/react-news-profiles-section/static/js/runtime-main.dd2a3710.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-news-profiles-section/precache-manifest.7f2bea1a6de2058472ba2d92f855aa64.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-news-profiles-section/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
